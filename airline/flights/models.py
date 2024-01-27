98a26d1e0c5e8232c637d43fcf511a6335fccbda
Huclark <vhuclark@gmail.com>
2024-01-27 22:12:24 +0000

    Update: Foreign key

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -12,8 +12,14 @@ class Airport(models.Model):
 
 class Flight(models.Model):
     # populate this model with all the data which will represent the model
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    # origin = models.CharField(max_length=64) => this was before Airport class
+    # we need to link the Airport table to the Flight table by using a foreign key
+    # CASCADE makes sure that a flight whose airport has been deleted from the 
+    # table will also be deleted in the flight table
+    # departures is the name given to link the airport
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    # destination = models.CharField(max_length=64)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
     def __str__(self):
