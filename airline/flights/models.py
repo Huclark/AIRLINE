f48f10780a38674441f1ddab0522d3b734a85bc3
Huclark <vhuclark@gmail.com>
2024-01-27 15:42:33 +0000

    Update: Airport class

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 # Every model is a python class
 
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f"{self.city} ({self.code})"
 
 class Flight(models.Model):
     # populate this model with all the data which will represent the model
