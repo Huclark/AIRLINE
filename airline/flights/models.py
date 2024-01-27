45b82ddd6fc1b9436a2477f37250664a2f36381c
Huclark <vhuclark@gmail.com>
2024-01-27 15:42:08 +0000

    Add: Flight class

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 # Every model is a python class
+
+
 class Flight(models.Model):
     # populate this model with all the data which will represent the model
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.duration}"
