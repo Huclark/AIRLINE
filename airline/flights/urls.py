db9fdb764d0d2ba9b486e07160dfa8014f5e93f1
Huclark <vhuclark@gmail.com>
2024-01-28 23:59:32 +0000

    Update: flight path

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # for every flight to have its own page do this:
+    path("<int:flight_id>", views.flight, name="flight")
 ]
