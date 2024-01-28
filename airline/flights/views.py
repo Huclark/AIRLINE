00271033f031030ac969f94492320a5702e26848
Huclark <vhuclark@gmail.com>
2024-01-28 23:59:52 +0000

    Update: flight method

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -7,3 +7,17 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+def flight(request, flight_id):
+    # retrieve the flight id from the Flight class
+    # pk means primary key
+    flight = Flight.objects.get(pk=flight_id)
+    # render a template, (flight.html) and pass flight
+    # as input to the flight.html template
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        # add passengers
+        # note that the passengers in flight.passengers.all()
+        # is the related_name
+        "passengers": flight.passenger.all()
+    })
