e7fe6e2f4e4385c89d08c9a5553862f588cc07b3
Huclark <vhuclark@gmail.com>
2024-01-29 16:26:07 +0000

    Update: flight path and booking path

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     # for every flight to have its own page do this:
-    path("<int:flight_id>", views.flight, name="flight")
+    path("<int:flight_id>", views.flight, name="flight"),
+    # add a route to book passengers for a flight
+    path("<int:flight_id>/book", views.book, name="book")
 ]
